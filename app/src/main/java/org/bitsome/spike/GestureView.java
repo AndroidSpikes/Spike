406b45829dfc3e07eb870d9f1ade6626bae65488
Claus Ljunggren <claus@ieee.org>
2015-09-28 20:58:54 +0200

    Clearly show direction in text and color

--- app/src/main/java/org/bitsome/spike/GestureView.java
+++ app/src/main/java/org/bitsome/spike/GestureView.java
@@ -12,6 +12,7 @@ public class GestureView extends View {
 
     private Paint paint;
     private float previousX, previousY;
+    private boolean clockwise = true;
 
     public GestureView(Context context) {
         this(context, null);
@@ -34,9 +35,10 @@ public class GestureView extends View {
                 if (event.getAction() == MotionEvent.ACTION_MOVE) {
                     float centerX = (float)getWidth()/2F;
                     float centerY = (float)getHeight()/2F;
-                    C.log(isClockwise(centerX, centerY, event.getX(), event.getY()) ? "Clockwise" : "Counter-Clockwise");
+                    clockwise = isClockwise(centerX, centerY, event.getX(), event.getY());
                     previousX = event.getX();
                     previousY = event.getY();
+                    invalidate();
                 }
                 return true;
             }
@@ -62,8 +64,26 @@ public class GestureView extends View {
         super.onDraw(canvas);
         if (paint == null) {
             paint = new Paint();
+            paint.setTextSize(160);
+            paint.setStrokeWidth(6);
         }
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(getWidth()/2, getHeight()/2, 20F, paint);
+
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 40F, paint);
+        if (clockwise) {
+            paint.setColor(Color.parseColor("#c8df8d"));
+            canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+            paint.setColor(Color.parseColor("#4f6f1a"));
+        } else {
+            paint.setColor(Color.parseColor("#e2b6b3"));
+            canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+            paint.setColor(Color.parseColor("#b32318"));
+        }
+        canvas.drawCircle(getWidth()/2,getHeight()/2, 40f,paint);
+        canvas.drawCircle(previousX, previousY, 40F, paint);
+        canvas.drawLine(previousX, previousY, getWidth() / 2, getHeight() / 2, paint);
+
+        String text = clockwise ?"Clockwise":"Counter";
+        float w = paint.measureText(text);
+        canvas.drawText(text, (getWidth() - w) / 2, getHeight()/4, paint);
     }
 }
